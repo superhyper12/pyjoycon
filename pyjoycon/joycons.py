59db61c74991fee2fa8e43bcab5ddabb1ac1485b
Egg Roll <84691248+superhyper12@users.noreply.github.com>
2022-12-27 00:47:29 -0500

    Add files via upload

---
+++ pyjoycon/joycons.py
@@ -0,0 +1,11 @@
+from pyjoycon import GyroTrackingJoyCon, get_R_id
+import time
+
+joycon_id = get_R_id()
+joycon = GyroTrackingJoyCon(*joycon_id)
+for i in range(20):
+    print("joycon pointer:  ", joycon.pointer)
+    print("joycon rotation: ", joycon.rotation)
+    print("joycon direction:", joycon.direction)
+    print()
+    time.sleep(0.05)
---
+++ pyjoycon/listdevice.py
@@ -0,0 +1,9 @@
+import hid
+import time
+
+for device_dict in hid.enumerate():
+    keys = list(device_dict.keys())
+    keys.sort()
+    for key in keys:
+        print("%s : %s" % (key, device_dict[key]))
+    print()
